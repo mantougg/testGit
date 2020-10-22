40fcc4f4c8f99f91fd9e5d3b126417c9b62ad2d2
mantougg <60961728+mantougg@users.noreply.github.com>
2020-10-22 22:01:01 +0800

    Update TestGit.java

--- src/TestGit.java
+++ src/TestGit.java
@@ -2,5 +2,7 @@ public class TestGit {
     public static void main(String[] args) {
         System.out.println("Hello Git!!!Hello");
         System.out.println("Hello Git!!!Hello");
+        System.out.println("Hello Git!!!Hello");
+        System.out.println("Hello Git!!!Hello");
     }
 }
