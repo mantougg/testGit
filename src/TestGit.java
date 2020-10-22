8518794411e9e3e8fb9e2c84104252022349759b
mantougg <60961728+mantougg@users.noreply.github.com>
2020-10-22 21:55:07 +0800

    Update TestGit.java

--- src/TestGit.java
+++ src/TestGit.java
@@ -1,10 +1,3 @@
-/**
- * @ClassName TestGit
- * @Description TODO
- * @Author Wang Yi
- * @Date 2020/10/22 21:17
- * @Version 1.0
- */
 public class TestGit {
     public static void main(String[] args) {
         System.out.println("Hello Git!!!");
